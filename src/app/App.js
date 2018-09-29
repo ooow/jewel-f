b4c8aaf27a40f623dc525c9991be20c1a1073e62
Goga Tirkiya <tirkiya@icloud.com>
2018-09-29 15:33:36 +0200

    enable scss

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
@@ -12,7 +12,7 @@ class App extends Component {
             className="App-logo"
             alt="logo"
           />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Jewel</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
