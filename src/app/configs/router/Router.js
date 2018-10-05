13b4c7e85e84ec09cdfc88b82f65c0b0da583818
Goga Tirkiya <tirkiya@icloud.com>
2018-10-06 00:45:51 +0200

    Configure routing with not found page hook

---
+++ src/app/configs/router/Router.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import App from '../../pages/temp-app/App';
+import NotFound from '../../pages/not-found/NotFound';
+
+/**
+ * Main project routing configuration.
+ */
+class Router extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={App} />,
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default Router;
