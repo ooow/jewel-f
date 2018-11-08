b24f303274b3026e52452c8bb72e4541f29d4638
Nikita Belonogov <belonogovna@sgu.ru>
2018-11-08 12:05:40 +0400

    update colors

--- src/app/components/NavBar/index.js
+++ src/app/components/NavBar/index.js
@@ -7,18 +7,18 @@ import Logo from '../Logo';
 class NavBar extends Component {
   render() {
     return (
-      <nav className='navbar navbar-light bg-light'>
+      <nav className='navbar navbar-light bg-white border-bottom'>
         <div className='container d-flex justify-content-between flex-sm-nowrap'>
           <a className='navbar-brand mx-1 p-0 d-none d-sm-inline' href='/'>
             <Logo style={{ height: '2em', width: '2em' }} />
           </a>
-          <div className='p-1 mx-1 my-1 my-sm-0 bg-white rounded text-nowrap w-100'>
+          <div className='p-1 mx-1 my-1 my-sm-0 bg-white rounded border text-nowrap text-muted w-100'>
             Search bar stub
           </div>
-          <div className='p-1 mx-1 my-1 my-sm-0 bg-white rounded text-nowrap'>
+          <div className='p-1 mx-1 my-1 my-sm-0 bg-white rounded border text-nowrap text-muted'>
             Language stub
           </div>
-          <div className='p-1 mx-1 my-1 my-sm-0 bg-white rounded text-nowrap'>
+          <div className='p-1 mx-1 my-1 my-sm-0 bg-white rounded border text-nowrap text-muted'>
             Sign in stub
           </div>
         </div>
