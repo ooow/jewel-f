580f4cbd5b3802aefdf1ffef61b2544f84957836
Goga Tirkiya <tirkiya@icloud.com>
2018-11-15 00:26:18 +0100

    implement local storage service

---
+++ src/app/services/LocalStoreService.js
@@ -0,0 +1,50 @@
+import window from 'global/window';
+import Language from '../models/Language';
+
+/**
+ * Tries to get value from local storage for provided key.
+ * @param key: {string}
+ * @private
+ */
+const read = (key) => {
+  let object;
+  try {
+    object = JSON.parse(window.localStorage.getItem(key));
+  } catch (e) {
+    /** TODO: Handle possible error. */
+    return null;
+  }
+  return object;
+};
+
+/**
+ * Saves to local storage object with key.
+ * @param key: {string}
+ * @param value: {any}
+ * @private
+ */
+const write = (key, value) => {
+  window.localStorage.setItem(key, JSON.stringify(value));
+};
+
+/**
+ * LocalStorage service provides methods for getting/setting the project
+ * variables from global local storage.
+ */
+export default class LocalStoreService {
+  /**
+   * Saves provided language into local storage.
+   * @param language: {!Language}
+   * @public
+   */
+  static setLanguage = (language) => {
+    write('language', language);
+  };
+
+  /**
+   * Reads language from local storage.
+   * @return {?Language}
+   * @public
+   */
+  static getLanguage = () => Language.toModel(read('language'));
+}
