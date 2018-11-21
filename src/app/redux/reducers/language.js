ff77636d46721ce1c5ff5d25d9c87820f24052fa
Goga Tirkiya <tirkiya@icloud.com>
2018-11-21 13:53:02 +0100

    LanguageSelector component (#79)

---
+++ src/app/redux/reducers/language.js
@@ -0,0 +1,24 @@
+import { CHANGE_LANGUAGE } from '../actions/language';
+import LocalStorageService from '../../services/LocalStoreService';
+import Language from '../../models/Language';
+
+let initState;
+const storedLanguage = LocalStorageService.getLanguage();
+
+if (storedLanguage) {
+  initState = storedLanguage;
+} else {
+  const defaultLanguage = new Language('en', 'English', true);
+  initState = defaultLanguage;
+  LocalStorageService.setLanguage(defaultLanguage);
+}
+
+const language = (state = initState, action) => {
+  if (action.type === CHANGE_LANGUAGE) {
+    LocalStorageService.setLanguage(action.language);
+    return action.language;
+  }
+  return state;
+};
+
+export default language;
